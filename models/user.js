d5ceb8fe3dacdbc2367641dde89df1ed6c4e175a
parkmiso <nomiso23@gmail.com>
2022-05-30 22:02:53 +0900

    10강까지 완료

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcyrpt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +31,22 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+//user 정보를 저장하기 전에 10강 참고
+userSchema.pre("save", function (next) {
+  var user = this; //this는 위에 schema 의미함
+  if (user.isModified("password")) {
+    //비밀번호 암호화 시킨다
+    bcyrpt.genSalt(saltRounds, function (error, salt) {
+      if (error) return next(error); //에러가 나면 index.js의 user.save로 돌려보냄
+
+      bcyrpt.hash(user.password, salt, function (error, hash) {
+        if (error) return next(error); //에러가 나면 index.js의 user.save로 돌려보냄
+        user.password = hash;
+        next(); //완성이 되면 돌아간다
+      });
+    });
+  }
 });
 
 const User = mongoose.model("User", userSchema);
